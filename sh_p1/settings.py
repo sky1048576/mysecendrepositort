7bf2de821293dafa11408da50ca150089aa0ef74
sky1048576 <shahabemame@yahoo.com>
2017-03-07 02:37:24 +0330

    fake static

--- sh_p1/settings.py
+++ sh_p1/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'django.contrib.humanize',
     'persons',
     'accounts',
@@ -131,8 +132,17 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'suggestions')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 LOGIN_URL = 'login'
 LOGUT_URL = 'logout'
 LOGIN_REDIRECT_URL = '/persons/yourHomes'
